e50e9b0bf2a5d5c27b09a9c2ed2c15073c0ece44
Vinicius <mbrozzgamer@hotmail.com>
2024-08-07 15:28:39 -0300

    Criação da extração de clientes

---
+++ customers/src/customers.py
@@ -0,0 +1,41 @@
+import redis
+import json
+import time
+import sys
+
+from Agregador import *
+
+# CONFIGURA O REDIS
+# DADOS CONFIGURAÇÃO REDIS
+rd = None
+env = {}
+infoConfig = {}
+try:
+    rd = redis.Redis("localhost", 6445)
+    envjs = rd.get("system_config")
+    if envjs is None:
+        print("CHAVE system_config NAO ENCONTRADA", flush=True)
+        time.sleep(10)
+        sys.exit(1)
+    env = json.loads(envjs)
+except Exception as e:
+    print("SERVICE REDIS DOWN: ", e, flush=True)
+    time.sleep(10)
+    sys.exit(1)
+    
+def main():
+    try:
+        api = 'customers'
+        url = f"https://api.sienge.com.br/{env['dominio']}/public/api/v1/{api}?"
+        creditors = Agregador(rd, api, url, env['sienge_user'], env['sienge_pwd'], env['dominio'])
+        dados = creditors.getData()
+        if dados:
+            print('Clientes salvos com sucesso!', flush=True)
+        else:
+            raise Exception('Erro ao salvar clientes!')
+        
+    except Exception as err:
+        print("Error on 'main' service: ", err, flush=True)
+        return None
+    
+main()
