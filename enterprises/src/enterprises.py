fe4b6c83a5bfac740b7db7a64c669b762c4149aa
Vinicius <mbrozzgamer@hotmail.com>
2024-08-09 14:49:27 -0300

    Todos agregadores agora seguem o mesmo modelo criado + testes para cada um deles.

--- enterprises/src/enterprises.py
+++ enterprises/src/enterprises.py
@@ -2,40 +2,128 @@ import redis
 import json
 import time
 import sys
+import datetime
 
 from Agregador import *
+from Exchange import *
+
+# VARIÁVEIS GLOBAIS
+dataInicio = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+incremento = 0
+result = {}
+api = 'enterprises'
+amqps= "amqps://sg_biapi:lvxJUm3GCHjhUJAJb6fdSxVUfcqzN@tall-cyan-dog.rmq4.cloudamqp.com/sg_biapi_host"
 
 # CONFIGURA O REDIS
 # DADOS CONFIGURAÇÃO REDIS
 rd = None
-env = {}
-infoConfig = {}
 try:
     rd = redis.Redis("localhost", 6445)
-    envjs = rd.get("system_config")
-    if envjs is None:
-        print("CHAVE system_config NAO ENCONTRADA", flush=True)
-        time.sleep(10)
-        sys.exit(1)
-    env = json.loads(envjs)
 except Exception as e:
     print("SERVICE REDIS DOWN: ", e, flush=True)
     time.sleep(10)
     sys.exit(1)
-    
-def main():
+
+# Exemplo de payload:
+# data:
+# {
+# "salesContractId": int,
+# "client": "bambui",
+# "service": "SALES_CONTRACT_CREATED",
+# "api": "sales-contracts",
+# "url": "bambui.sienge.com.br",
+# "user": "psa",
+# "pass": "secret"
+# }
+
+def callback(ch, method, properties, body):
     try:
-        api = 'enterprises'
-        url = f"https://api.sienge.com.br/{env['dominio']}/public/api/v1/{api}?"
-        extraction = Agregador(rd, api, url, env['sienge_user'], env['sienge_pwd'], env['dominio'])
-        dados = extraction.getData()
-        if dados:
-            print('Empreendimentos salvos com sucesso!', flush=True)
+        global incremento, result, api, dataInicio
+        # Independente, libera a mensagem da fila
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+        
+        # Incrementa o contador de acessos, quantas vezes esse agregador rodou.
+        incremento = incremento + 1
+        
+        # JSON de resultado da execução
+        result = {
+            "dataInicio": dataInicio,
+            "dataUltimoAcesso": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            "api": api,
+            "acessos": incremento,
+            "status": "DONE",
+        }
+        
+        # Fazer o tratamento do body
+        msg = body.decode("utf-8")
+        message = json.loads(msg)
+        payload = message.get('payload')
+        hmac = message.get('hmac')
+        
+        # Verifica se o payload é válido
+        if exchange.getPayload(hmac, payload):
+            print(f"[x] Payload recebido [{payload}]\n", flush=True)
+            print(exchange.payload, flush=True)
+            msg = json.loads(exchange.payload)
+        
+            if api != msg['api'].lower():
+                print(f"API {api} não é a mesma da mensagem recebida", flush=True)
+                raise Exception(f"API {api} não é a mesma da mensagem recebida")
+        
+        # Pegando a primeira posição do split da URL, que será o domínio
+        # bambui.sienge.com.br -> bambui
+        dominio = msg['url'].split(".")[0]
+        
+        # URL da API
+        url = f"https://api.sienge.com.br/{dominio}/public/api/v1/{api}?"
+        
+        # Instanciando a classe do Agregador
+        extraction = Agregador(rd, api, url, msg['user'], msg['pass'], dominio)
+        
+        status, retorno = extraction.getData()
+        
+        # Verifica se o status é True
+        if status:
+            retorno['payload'] = msg
+            payloadRetorno = json.dumps(retorno)
+            # Momento em que eu criptgrafo o retorno e ele fica em exchange.payload
+            # Porém ele ainda não foi enviado, apenas criptografado e aguardando um sendMsg.
+            exchange.setPayload(payloadRetorno)
+            # Enviando a mensagem, além de criptografada, envia o 'result' que contém informações para métricas.
+            print(f'Enviando a mensagem: {json.dumps(result)}', flush=True)
+            exchange.sendMsg(json.dumps(result))
+            
         else:
-            raise Exception('Erro ao salvar empreendimentos!')
+            # Se o status for False, retorna o JSON que a função getData retornou
+            raise Exception(retorno)
         
     except Exception as err:
-        print("Error on 'main' service: ", err, flush=True)
-        return None
+        # Se ocorrer algum erro, retorna o erro
+        result['body'] = body.decode("utf-8")
+        result['status'] = "ERROR"
+        result['error'] = str(err)
+        print(f"Erro ao extrair: {result}", flush=True)
+        exchange.sendMsg(json.dumps(result))
+        
+# INICIA O CONSUMO DA FILA + INSTANCIAMENTO DO EXCHANGE
+serviceid  = '01234567890123456789012345678901'
+servicekey = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456'
+host       = 'vini'
+version    = '1.0 08/08/2024 Vini - Exemplo de uso empreendimentos.'
+service    = 'agregador_enterprises'
+system     = 'sg_biapi'
+try:
+    exchange = QueueExchange(
+        amqps   = amqps,
+        host    = host,
+        system  = system, 
+        service = service,
+        version = version,
+        callback= callback)
+except Exception as ex:
+    print("[!] rabbitmq service offline", amqps, ex, flush=True)
+    sys.exit(0)
     
-main()
+exchange.setCrypto(serviceid, servicekey)
+
+exchange.start_consuming()
---
+++ units/src/testeUnits.py
@@ -0,0 +1,37 @@
+import time, json, sys
+
+import pprint
+
+from Exchange import *
+
+serviceid  = '01234567890123456789012345678901'
+servicekey = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456'
+host       = 'vini'
+version    = '1.0 08/08/2024 Vini - Exemplo de uso empresas'
+service    = 'agregador_units'
+system     = 'sg_biapi'
+
+amqps= "amqps://sg_biapi:lvxJUm3GCHjhUJAJb6fdSxVUfcqzN@tall-cyan-dog.rmq4.cloudamqp.com/sg_biapi_host"
+try:
+    exchange = QueueExchange(
+        amqps    = amqps,
+        host     = host,
+        system   = system, 
+        service  = service, 
+        version  = version)
+except Exception as ex:
+    print("[!] Exchange offline", ex)
+    sys.exit(0)
+
+payload = {
+    "companyId": 1,
+    "client": "bambui",
+    "api": "units",
+    "url": "bambui.sienge.com.br",
+    "user": "bambui-psad",
+    "pass": "STPOi4bCpz0UuZfaCNB7dEjQQKjqrbeM"
+}
+
+exchange.setCrypto(serviceid, servicekey)
+exchange.setPayload(json.dumps(payload))
+exchange.sendMsg('ESTOU ENVIANDO UMA MSG SECRETA', service)
